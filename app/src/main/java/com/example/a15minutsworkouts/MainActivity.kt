cd6e1ae9ec6607a33a4cf174c2e1aea7888e3d86
Krishna Chaudhary <krishna.chaudhary@hapana.com>
2023-10-06 09:01:36 +0530

    create activity

---
+++ app/src/main/java/com/example/a15minutsworkouts/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.a15minutsworkouts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.a15minutsworkouts.databinding.MainActivityBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding : MainActivityBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        binding?.flstart?.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Click", Toast.LENGTH_SHORT ).show()
+
+            val intent = Intent(this, WorkoutList::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
