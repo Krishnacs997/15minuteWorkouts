414e77144eb930d535863ee95b93c53acf55a79f
Krishna Chaudhary <krishna.chaudhary@hapana.com>
2023-10-17 23:20:41 +0530

    changes

--- app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
@@ -11,6 +11,7 @@ import com.example.a7minuteworkout.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
 
